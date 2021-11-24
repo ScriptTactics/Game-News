27204841eec9078aeeb2694e46a946be924b4736
Jackson Rosenthal <pwrxpsycho@gmail.com>
2021-11-24 00:11:39 -0500

    Fixed subscribe function

--- src/commands/subscribe.ts
+++ src/commands/subscribe.ts
@@ -5,7 +5,7 @@ import { subscriptionList } from "..";
 export = {
     name: 'subscribe',
     description: 'Subscribe to Game News',
-    execute(commandArgs: CommandArgs) {
+    async execute(commandArgs: CommandArgs) {
 
         const gameID = commandArgs.msg.content.split(' ')[1];
 
@@ -13,11 +13,35 @@ export = {
             return commandArgs.msg.channel.send('Game ID can only be digits');
         }
         try {
-            fs.appendFileSync(subscriptionList, gameID +'\n');
+            const rl = fs.createReadStream(subscriptionList, {
+                flags: 'a+',
+                encoding: 'utf8'
+            });
+            let duplicate = false;
+            rl.on('error', (err) => { throw err; });
+            rl.on('data', (data) => {
+                const found = data.toString().split('\n').find(value => { return value === gameID });
+                if (found) {
+                    duplicate = true;
+                    return commandArgs.msg.channel.send('You are already subscribed to that');
+
+                }
+
+
+            });
+            rl.on('end', () => {
+                if (!duplicate) {
+                    const file = fs.createWriteStream(subscriptionList, { flags: 'a+' });
+                    file.on('error', (err) => { throw err; });
+                    file.write(gameID + '\n');
+                    file.end();
+                    commandArgs.msg.channel.send('Succesfully Subscribed');
+                }
+            });
+
         } catch (error) {
             return error;
         }
-        commandArgs.msg.channel.send('Succesfully Subscribed');
     }
 
 } as Command;
